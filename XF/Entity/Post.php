f2e4b3808942ac1da11c5437c1f12a784d38fc75
Fortree Forums <rennofthemill@hotmail.co.uk>
2021-07-17 20:35:28 +0100

    Don't increase activity if node_id is excluded

--- XF/Entity/Post.php
+++ XF/Entity/Post.php
@@ -8,8 +8,15 @@ class Post extends XFCP_Post
 {
 	protected function adjustUserMessageCountIfNeeded($amount)
 	{
+		$nodeId = $this->Thread->node_id;
+		$options = \XF::options();
 		$parent = parent::adjustUserMessageCountIfNeeded($amount);
 		
-		$this->User->fastUpdate('ap_activity_meter', max(0, $this->User->ap_activity_meter + $amount));
+		$forums = $options->ap_actlong_exclude_nodes;
+		
+		if(!in_array($nodeId, $forums))
+		{
+			$this->User->fastUpdate('ap_activity_meter', max(0, $this->User->ap_activity_meter + $amount));
+		}
 	}
 }
