c373c22511cd66ff69e42ac7e69e8c59dd6c702f
Fortree Forums <rennofthemill@hotmail.co.uk>
2021-07-21 20:21:28 +0100

    Add support for [AP] Minimum Characters for Post Count

--- XF/Entity/Post.php
+++ XF/Entity/Post.php
@@ -14,9 +14,24 @@ class Post extends XFCP_Post
 		
 		$forums = $options->ap_actlong_exclude_nodes;
 		
+		/* Check if [AP] Minimum Characters for Post Count is installed */
+		/* and only increase the meters level if the charcount check is met */
+		$addons = \XF::app()->container('addon.cache');
+		
 		if(!in_array($nodeId, $forums))
 		{
-			$this->User->fastUpdate('ap_activity_meter', max(0, $this->User->ap_activity_meter + $amount));
+			if(array_key_exists('apathy/MinChars', $addons) 
+			&& $addons['apathy/MinChars'] >= 1000070)
+			{
+				if($this->chars >= $options->ap_char_limit)
+				{
+					$this->User->fastUpdate('ap_activity_meter', max(0, $this->User->ap_activity_meter + $amount));
+				}
+			}
+			else
+			{
+				$this->User->fastUpdate('ap_activity_meter', max(0, $this->User->ap_activity_meter + $amount));
+			}
 		}
 	}
 }
