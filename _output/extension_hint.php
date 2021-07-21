c6f0f3f51663adbf60ef62e68f6f913070e84a57
Fortree Forums <rennofthemill@hotmail.co.uk>
2021-07-22 00:35:32 +0100

    Delete _output directory

--- _output/extension_hint.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// ################## THIS IS A GENERATED FILE ##################
-// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
-
-namespace apathy\ActLong\XF\Entity
-{
-	class XFCP_Post extends \XF\Entity\Post {}
-}
