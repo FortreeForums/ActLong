964699598daa836163e2d8511f8af5ba09b3e3e4
Fortree Forums <rennofthemill@hotmail.co.uk>
2021-07-22 14:18:58 +0100

    Misc changes
    - Fix license
    - Ensure the user exists & discussion_state = visible before increasing post count

--- Setup.php
+++ Setup.php
@@ -8,72 +8,31 @@
 //  https://fortreeforums.xyz
 //  Licensed under GPL-3.0-or-later 2021
 //
-//  This file is part of [AP] Activity/Longevity Meters ("ActLong").
+//  This file is part of [AP] Minimum Characters for Post Count ("MinChars").
 //
-//  ActLong is free software: you can redistribute it and/or modify
+//  MinChars is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
 //
-//  ActLong is distributed in the hope that it will be useful,
+//  MinChars is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
 //
 //  You should have received a copy of the GNU General Public License
-//  along with ActLong.  If not, see <https://www.gnu.org/licenses/>.
+//  along with MinChars.  If not, see <https://www.gnu.org/licenses/>.
 
-namespace apathy\ActLong;
+namespace apathy\MinChars;
 
 use XF\AddOn\AbstractSetup;
 use XF\AddOn\StepRunnerInstallTrait;
 use XF\AddOn\StepRunnerUninstallTrait;
 use XF\AddOn\StepRunnerUpgradeTrait;
-use XF\Db\Schema\Alter;
 
 class Setup extends AbstractSetup
 {
 	use StepRunnerInstallTrait;
 	use StepRunnerUpgradeTrait;
 	use StepRunnerUninstallTrait;
-	
-	public function installStep1()
-	{
-		$this->schemaManager()->alterTable('xf_user', function(Alter $table)
-		{
-			$table->addColumn('ap_activity_meter', 'int')->nullable()->setDefault(null);
-		});
-	}
-	
-	public function uninstallStep1()
-	{
- 		$this->schemaManager()->alterTable('xf_user', function(Alter $table)
-    		{
-			$table->dropColumns('ap_activity_meter');
-   		});
-	}
-	
-	public function installStep2()
-	{
-		/* Fetch User ID 1's registration date
-		   and store it in SimpleCache.
-		   This will be used as a "start date" for
-		   the forum, and Longevity meters are
-		   based around this value. */
-		   
-		   $app = \XF::app();
-		   $finder = \XF::finder('XF:User');
-		   $date = $finder->where('user_id', 1)->fetchOne()->register_date;
-		   $simpleCache = $app->simpleCache();
-		   
-		   $simpleCache['apathy/ActLong']['forum_start'] = $date;
-	}
-	
-	public function uninstallStep2()
-	{
-		   $app = \XF::app();
-		   $simpleCache = $app->simpleCache();
-		   
-		   $simpleCache['apathy/ActLong']['forum_start'] = NULL;		
-	}
 }
--- XF/Entity/Post.php
+++ XF/Entity/Post.php
@@ -8,53 +8,87 @@
 //  https://fortreeforums.xyz
 //  Licensed under GPL-3.0-or-later 2021
 //
-//  This file is part of [AP] Activity/Longevity Meters ("ActLong").
+//  This file is part of [AP] Minimum Characters for Post Count ("MinChars").
 //
-//  ActLong is free software: you can redistribute it and/or modify
+//  MinChars is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
 //
-//  ActLong is distributed in the hope that it will be useful,
+//  MinChars is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
 //
 //  You should have received a copy of the GNU General Public License
-//  along with ActLong.  If not, see <https://www.gnu.org/licenses/>.
+//  along with MinChars.  If not, see <https://www.gnu.org/licenses/>.
 
-namespace apathy\ActLong\XF\Entity;
+namespace apathy\MinChars\XF\Entity;
 
 use XF\Mvc\Entity\Structure;
 
 class Post extends XFCP_Post
 {
+	protected $chars;
+	
 	protected function adjustUserMessageCountIfNeeded($amount)
 	{
-		$nodeId = $this->Thread->node_id;
 		$options = \XF::options();
-		$parent = parent::adjustUserMessageCountIfNeeded($amount);
 		
-		$forums = $options->ap_actlong_exclude_nodes;
-		
-		/* Check if [AP] Minimum Characters for Post Count is installed */
-		/* and only increase the meters level if the charcount check is met */
-		$addons = \XF::app()->container('addon.cache');
-		
-		if(!in_array($nodeId, $forums))
+		if ($this->user_id
+		&& $this->User
+		&& $this->Thread->discussion_state == 'visible')
 		{
-			if(array_key_exists('apathy/MinChars', $addons) 
-			&& $addons['apathy/MinChars'] >= 1000070)
+			$excludedForums = $options->ap_minchars_exclude_nodes;
+			$user_ids = $options->ap_minchars_exclude_users;
+			$excludedUsers = explode(",", $user_ids);
+			$message = $this->message;
+			
+			if(in_array($this->Thread->node_id, $excludedForums) 
+			|| in_array($this->user_id, $excludedUsers))
 			{
-				if($this->chars >= $options->ap_char_limit)
-				{
-					$this->User->fastUpdate('ap_activity_meter', max(0, $this->User->ap_activity_meter + $amount));
-				}
+				$this->chars = $options->ap_char_limit;
 			}
 			else
 			{
-				$this->User->fastUpdate('ap_activity_meter', max(0, $this->User->ap_activity_meter + $amount));
+				if($options->ap_quote_check)
+				{
+					/* Strip quoted text to stop inflation */
+					/* Regex found on StackOverflow */
+					/* https://stackoverflow.com/a/7208743 */
+					$message = preg_replace('/\[quote=(.*?)\](((?R)|.*?)+)\[\/quote\]/is', '', $message);
+				}
+
+				if($options->ap_bbcode_check)
+				{
+					/* Strip BBCode tags to stop inflation */	
+					$message = preg_replace('#\[[^]]*\]#', '', $message);
+				}
+			
+				if($options->ap_url_check)
+				{
+					/* Strip URLs to stop inflation */
+					/* Regex found on StackOverflow */
+					/* https://stackoverflow.com/a/54808354 */
+					$message = preg_replace('#((\w+:\/\/\S+)|(\w+[\.:]\w+\S+))[^\s,\.]#is', '', $message);
+				}
+			
+				if($options->ap_newline_check)
+				{
+					/* Strip newlines to stop inflation */
+					$message = preg_replace('/(\s)*/', '', $message);
+				}
+						
+				/* Finally count the characters */
+				$this->chars = strlen($message);
 			}
+			
+			/* Only update if message is greater than X chars */
+			if($this->chars >= $options->ap_char_limit)
+			{
+				$this->User->fastUpdate('message_count', max(0, $this->User->message_count + $amount));
+			}	
 		}
 	}
 }
+
