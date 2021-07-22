940387e9382acb5bbb5cb5d57417fe73a09d8c4e
Fortree Forums <rennofthemill@hotmail.co.uk>
2021-07-22 12:58:20 +0100

    Only run if ap_actlong_disable_activity = 0

--- Cron/Activity.php
+++ Cron/Activity.php
@@ -29,14 +29,19 @@ class Activity
 {
 	public static function resetActivityMeter()
 	{
-		$finder = \XF::finder('XF:User');
-		$users = $finder->where('user_state', 'valid')
-				->where('ap_activity_meter', '>=', 1)
-				->fetch();
+		$options = \XF::options();
 		
-		foreach($users as $user)
+		if(!$options->ap_actlong_disable_activity)
 		{
-			$user->fastUpdate('ap_activity_meter', 0);
+			$finder = \XF::finder('XF:User');
+			$users = $finder->where('user_state', 'valid')
+					->where('ap_activity_meter', '>=', 1)
+					->fetch();
+		
+			foreach($users as $user)
+			{
+				$user->fastUpdate('ap_activity_meter', 0);
+			}
 		}
 	}
 }
